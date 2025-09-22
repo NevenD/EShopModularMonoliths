ac5e701f6c30874fda5644ca7288ad8f707f1b96
Neven Dimač <neven.dimac@gmail.com>
2025-09-22 20:05:50 +0200

    Develop shared domain for tactical DDD abstraction

---
+++ src/Shared/Shared/DDD/Aggregate.cs.cs
@@ -0,0 +1,26 @@
+﻿namespace Shared.DDD
+{
+    public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+    {
+        // Internal list to store domain events raised by the aggregate.
+        private readonly List<IDomainEvent> _domainEvents = [];
+
+        // Exposes domain events as a read-only list for external consumers.
+        public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+        // Adds a domain event to the aggregate's event list.
+        public void AdDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        // Clears all domain events from the aggregate and returns them as an array.
+        public IDomainEvent[] ClearDomainEvents()
+        {
+            IDomainEvent[] dequedEvents = _domainEvents.ToArray();
+            _domainEvents.Clear();
+
+            return dequedEvents;
+        }
+    }
+}
