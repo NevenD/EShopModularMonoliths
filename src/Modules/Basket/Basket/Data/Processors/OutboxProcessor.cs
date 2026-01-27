703c20ed84b5270570e46ba56be5c6494b506817
Neven Dimač <neven.dimac@gmail.com>
2026-01-27 19:40:38 +0100

    Fix outbox processor

--- src/Modules/Basket/Basket/Data/Processors/OutboxProcessor.cs
+++ src/Modules/Basket/Basket/Data/Processors/OutboxProcessor.cs
@@ -5,64 +5,53 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System.Text.Json;
 
-namespace Basket.Data.Processors
+namespace Basket.Data.Processors;
+public class OutboxProcessor
+    (IServiceProvider serviceProvider, IBus bus, ILogger<OutboxProcessor> logger)
+    : BackgroundService
 {
-    public class OutboxProcessor : BackgroundService
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        private readonly IServiceProvider _serviceProvider;
-        private readonly IBus _bus;
-        private readonly ILogger _logger;
-
-        public OutboxProcessor(IServiceProvider serviceProvider, IBus bus, ILogger logger)
+        while (!stoppingToken.IsCancellationRequested)
         {
-            _serviceProvider = serviceProvider;
-            _bus = bus;
-            _logger = logger;
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            while (!stoppingToken.IsCancellationRequested)
+            try
             {
-                try
+                using var scope = serviceProvider.CreateScope();
+                var dbContext = scope.ServiceProvider.GetRequiredService<BasketDbContext>();
+                var outboxMessages = await dbContext.OutboxMessages
+                    .Where(m => m.ProcessedOn == null)
+                    .ToListAsync(stoppingToken);
+
+                foreach (var message in outboxMessages)
                 {
-                    using var scope = _serviceProvider.CreateScope();
-                    var dbContext = scope.ServiceProvider.GetRequiredService<BasketDbContext>();
-                    var outboxMessages = await dbContext.OutboxMessages
-                        .Where(m => m.ProcessedOn == null)
-                        .ToListAsync(stoppingToken);
-
-                    foreach (var message in outboxMessages)
+                    var eventType = Type.GetType(message.Type);
+                    if (eventType == null)
                     {
-                        var eventType = Type.GetType(message.Type);
-                        if (eventType == null)
-                        {
-                            _logger.LogWarning("Could not resolve type: {Type}", message.Type);
-                            continue;
-                        }
-                        var eventMessage = JsonSerializer.Deserialize(message.Content, eventType);
-                        if (eventMessage == null)
-                        {
-                            _logger.LogWarning("Could not deserialize message: {Content}", message.Content);
-                            continue;
-                        }
-
-                        await _bus.Publish(eventMessage, stoppingToken);
-
-                        message.ProcessedOn = DateTime.UtcNow;
-
-                        _logger.LogInformation("Successfully processed outbox message with ID: {Id}", message.Id);
+                        logger.LogWarning("Could not resolve type: {Type}", message.Type);
+                        continue;
+                    }
+                    var eventMessage = JsonSerializer.Deserialize(message.Content, eventType);
+                    if (eventMessage == null)
+                    {
+                        logger.LogWarning("Could not deserialize message: {Content}", message.Content);
+                        continue;
                     }
 
-                    await dbContext.SaveChangesAsync(stoppingToken);
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, "Error processing outbox messages");
+                    await bus.Publish(eventMessage, stoppingToken);
+
+                    message.ProcessedOn = DateTime.UtcNow;
+
+                    logger.LogInformation("Successfully processed outbox message with ID: {Id}", message.Id);
                 }
 
-                await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken); // Adjust the delay as needed
+                await dbContext.SaveChangesAsync(stoppingToken);
             }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error processing outbox messages");
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken); // Adjust the delay as needed
         }
     }
 }
